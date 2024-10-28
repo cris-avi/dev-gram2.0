1ce1e7695cc2e34ed809e76674c8e71d3c6b8fb6
cris-avi <cr.avilap@duocuc.cl>
2024-10-28 19:25:23 -0300

    commit 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCTca71omdlASYj_kbb6rZJvQCsgoHFgVM",
+    authDomain: "dev-gram2.firebaseapp.com",
+    projectId: "dev-gram2",
+    storageBucket: "dev-gram2.appspot.com",
+    messagingSenderId: "148503371474",
+    appId: "1:148503371474:web:5522604dd619eef2f6170b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCTca71omdlASYj_kbb6rZJvQCsgoHFgVM",
+    authDomain: "dev-gram2.firebaseapp.com",
+    projectId: "dev-gram2",
+    storageBucket: "dev-gram2.appspot.com",
+    messagingSenderId: "148503371474",
+    appId: "1:148503371474:web:5522604dd619eef2f6170b"
+  }
 };
 
 /*
