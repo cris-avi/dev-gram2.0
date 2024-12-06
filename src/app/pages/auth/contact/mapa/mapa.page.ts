751d3f37aa92e89a930dba3d27fa6f04c02f03a0
cris-avi <cr.avilap@duocuc.cl>
2024-12-05 21:12:21 -0300

    Punto de aparicion y indicador de dirección

--- src/app/pages/auth/contact/mapa/mapa.page.ts
+++ src/app/pages/auth/contact/mapa/mapa.page.ts
@@ -1,6 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 
-import { GoogleMap } from '@capacitor/google-maps';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
 import { environment } from 'src/environments/environment.prod';
 const apiKey = environment.firebaseConfig.apiKey;
 
@@ -25,20 +25,48 @@ export class MapaPage implements OnInit {
   }
 
   async initMap(){
+
     
     this.map = await GoogleMap.create({
       id: 'my-map', // Unique identifier for this map instance
       element: document.getElementById('map'), // reference to the capacitor-google-map element
       apiKey: apiKey, // Your Google Maps API Key
+      language: 'es',
       config: {
         center: {
           // The initial position to be rendered by the map
-          lat: 33.6,
-          lng: -117.9,
+          lat: 
+          -33.50012932189626,
+          lng: 
+          -70.61656594276428
         },
-        zoom: 8, // The initial zoom level to be rendered by the map
+        zoom: 18, // The initial zoom level to be rendered by the map
       },
     });
+
+    this.setMarkerDemo();
+    
+
+
+
+    this.map.setOnMapClickListener( res => {
+      console.log('setOnMapClickListener ->' , res);
+    })
+
+    
+  }
+
+  setMarkerDemo(){
+    const marker: Marker ={
+      coordinate: {
+        lat: 
+        -33.50012932189626,
+        lng: 
+        -70.61656594276428
+      }
+
+    }
+    this.map.addMarker(marker)
   }
 
 }
